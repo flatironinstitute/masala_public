f8b598fbf8a816bb7eea2c276e94d408d3623a13
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-08-04 03:08:02 -0400

    Initial commit of Molecule class.

---
+++ src/core/chemistry/Molecule.cc
@@ -0,0 +1,64 @@
+/*
+MIT License
+
+Copyright (c) 2022 Vikram K. Mulligan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/// @file src/core/chemistry/Molecule.cc
+/// @brief A class contiaining a collection of atoms and chemical bonds.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Class header:
+#include <core/chemistry/Molecule.hh>
+
+namespace core {
+namespace chemistry {
+
+/// @brief Clone operation: make a copy of this object and return a shared pointer
+/// to the copy.
+MoleculeSP
+Molecule::clone() const {
+    return std::make_shared< Molecule >( *this );
+}
+
+/// @brief Deep clone operation: make a deep copy of this object and return a shared
+/// pointer to the deep copy.
+MoleculeSP
+Molecule::deep_clone() const {
+    MoleculeSP molecules_copy( std::make_shared< Molecule >( *this ) );
+    molecules_copy->make_independent();
+    return molecules_copy;
+}
+
+/// @brief Make this object independent by making a deep copy of all of its private members.
+/// @details Be sure to update this function whenever a private member is added!
+void
+Molecule::make_independent() {
+    // Currently GNDN.
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC ACCESSORS
+////////////////////////////////////////////////////////////////////////////////
+
+
+} // namespace chemistry
+} // namespace core
---
+++ src/core/chemistry/Molecule.hh
@@ -0,0 +1,98 @@
+/*
+MIT License
+
+Copyright (c) 2022 Vikram K. Mulligan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/// @file src/core/chemistry/Molecule.hh
+/// @brief A class contiaining a collection of atoms and chemical bonds.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Masala_src_core_chemistry_Molecule_hh
+#define Masala_src_core_chemistry_Molecule_hh
+
+// Forward declarations:
+#include <core/chemistry/Molecule.fwd.hh>
+
+// Base headers:
+#include <base/MasalaObject.hh>
+
+// STL headers:
+#include <set>
+
+namespace core {
+namespace chemistry {
+
+
+
+/// @brief A class contiaining a collection of atoms and chemical bonds.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+class Molecule : public base::MasalaObject {
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTRUCTION, DESTRUCTION, AND CLONING
+////////////////////////////////////////////////////////////////////////////////
+
+    /// @brief Default constructor.
+    Molecule() = default;
+
+    /// @brief Copy constructor.
+    Molecule( Molecule const & ) = default;
+
+    /// @brief Default destructor.
+    ~Molecule() override = default;
+
+    /// @brief Clone operation: make a copy of this object and return a shared pointer
+    /// to the copy.
+    virtual
+    MoleculeSP
+    clone() const;
+
+    /// @brief Deep clone operation: make a deep copy of this object and return a shared
+    /// pointer to the deep copy.
+    virtual
+    MoleculeSP
+    deep_clone() const;
+
+    /// @brief Make this object independent by making a deep copy of all of its private members.
+    /// @details Be sure to update this function whenever a private member is added!
+    virtual
+    void
+    make_independent();
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC ACCESSORS
+////////////////////////////////////////////////////////////////////////////////
+
+
+private:
+
+
+};
+
+} // namespace chemistry
+} // namespace core
+
+#endif // Masala_src_core_chemistry_Molecule_hh
