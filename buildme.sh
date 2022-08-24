0f849b8d8fe85571c8902c2a408a553f29fafcf8
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-08-24 01:22:33 -0400

    Using generate_cmake_build.py.

--- buildme.sh
+++ buildme.sh
@@ -1,6 +1,10 @@
 mkdir bin
 mkdir build
 cd build
+mkdir cmake_generated
+python3 ../cmake/generate_cmake_build.py ../src/base cmake_generated/base.cmake
+python3 ../cmake/generate_cmake_build.py ../src/numeric cmake_generated/numeric.cmake
+python3 ../cmake/generate_cmake_build.py ../src/core cmake_generated/core.cmake
 cmake ../cmake/
 make . -j `nproc`
 cd ..
