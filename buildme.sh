3d0ec4010f5ab9b8cc17371358a80a12c841ba90
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-08-24 15:14:47 -0400

    Moving call of Python script to CMakeLists.txt.

--- buildme.sh
+++ buildme.sh
@@ -1,10 +1,7 @@
-mkdir bin
-mkdir build
+mkdir bin && echo "Created bin/ directory." || echo "Directory bin/ already exists."
+mkdir build && echo "Created build/ directory." || echo "Directory build/ already exists."
 cd build
-mkdir cmake_generated
-python3 ../cmake/generate_cmake_build.py base ../src/base cmake_generated/base.cmake
-python3 ../cmake/generate_cmake_build.py numeric ../src/numeric cmake_generated/numeric.cmake
-python3 ../cmake/generate_cmake_build.py core ../src/core cmake_generated/core.cmake
+mkdir cmake_generated && echo "Created build/cmake_generated/ sub-directory." || echo "Sub-directory build/cmake_generated/ already exists."
 cmake ../cmake/
 make -j `nproc` && echo "Build completed.  Executables may be found in the bin/ directory." || echo "Build errors!  Check output log."
 cd ..
