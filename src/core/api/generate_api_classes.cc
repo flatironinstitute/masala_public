7daf767e71a701114a8ab84a0905302f7a7ba246
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-08-31 18:07:42 -0400

    Functions to generate API classes.

---
+++ src/core/api/generate_api_classes.cc
@@ -0,0 +1,54 @@
+/*
+MIT License
+
+Copyright (c) 2022 Vikram K. Mulligan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/// @file src/core/api/generate_api_classes.cc
+/// @brief Utility functions to generate all of the classes in core that define
+/// an API.  Used for auto-generating the core api.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Project header
+#include <core/api/generate_api_classes.hh>
+
+// Core headers
+#include <core/pose/Pose.hh>
+
+namespace core {
+namespace api {
+
+    /// @brief Generate an instance of each of the classes in core that have a defined API, and
+    /// return a vector of owning pointers to these instances.
+    /// @details Used for auto-generating the core API description (as a JSON file).
+    std::vector< base::MasalaObjectSP >
+    generate_api_classes() {
+        std::vector< base::MasalaObjectSP > outvec;
+
+        // Add to this vector whenever a class is added with a defined API:
+        outvec.emplace_back( std::make_shared< core::pose::Pose >() );
+        // ADD MORE ENTRIES HERE
+
+        return outvec;
+    }
+
+} // namespace api
+} // namespace core
