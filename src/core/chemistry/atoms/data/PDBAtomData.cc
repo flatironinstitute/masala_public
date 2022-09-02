0a5f8976dc7357c5788d1b4b9f0254a317935eb5
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-09-02 16:28:14 -0400

    Initial commit of PDBAtomData class.

---
+++ src/core/chemistry/atoms/data/PDBAtomData.cc
@@ -0,0 +1,82 @@
+/*
+MIT License
+
+Copyright (c) 2022 Vikram K. Mulligan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/// @file src/core/chemistry/atoms/data/PDBAtomData.cc
+/// @brief A container class for data attached to an atom that was read from a PDB file.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Class header:
+#include <core/chemistry/atoms/data/PDBAtomData.hh>
+
+// Core headers:
+#include <core/chemistry/atoms/AtomInstance.hh>
+#include <core/chemistry/atoms/AtomCoordinateRepresentation.hh>
+#include <core/chemistry/bonds/ChemicalBondInstance.hh>
+
+// STL headers:
+#include <string>
+
+namespace core {
+namespace chemistry {
+namespace atoms {
+namespace data {
+
+/// @brief Clone operation: make a copy of this object and return a shared pointer
+/// to the copy.
+PDBAtomDataSP
+PDBAtomData::clone() const {
+    return std::make_shared< PDBAtomData >( *this );
+}
+
+/// @brief Deep clone operation: make a deep copy of this object and return a shared
+/// pointer to the deep copy.
+PDBAtomDataSP
+PDBAtomData::deep_clone() const {
+    PDBAtomDataSP pdbdata_copy( std::make_shared< PDBAtomData >( *this ) );
+    pdbdata_copy->make_independent();
+    return pdbdata_copy;
+}
+
+/// @brief Make this object independent by making a deep copy of all of its private members.
+/// @details Threadsafe.  Be sure to update this function whenever a private member is added!
+void
+PDBAtomData::make_independent() {
+}
+
+/// @brief Returns "PDBAtomData".
+std::string
+PDBAtomData::class_name() const {
+    return "PDBAtomData";
+}
+
+/// @brief Every class can provide its own namespace.  This returns "core::chemistry::atoms::data".
+std::string
+PDBAtomData::class_namespace() const {
+    return "core::chemistry::atoms::data";
+}
+
+} // namespace data
+} // namespace atoms
+} // namespace chemistry
+} // namespace core
