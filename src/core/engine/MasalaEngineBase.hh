c9617291e95172d639776268c175325076deae92
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-09-11 19:12:25 -0400

    Initial commit of MasalaEngineBase (base class for Masala engines for hard calculations).

---
+++ src/core/engine/MasalaEngineBase.hh
@@ -0,0 +1,78 @@
+/*
+MIT License
+
+Copyright (c) 2022 Vikram K. Mulligan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/// @file src/core/engine/MasalaEngineBase.hh
+/// @brief A base class for Masala engines, which perform hard calculations of a given type using
+/// a given method.
+/// @details Subclasses will be defined for kinematic calculations, packing calculations, minimization
+/// calculations, etc.  Each of these in turn will have subclasses for performing these using different
+/// math libraries, optimizers, hardware, etc.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Masala_src_core_engine_MasalaEngineBase_hh
+#define Masala_src_core_engine_MasalaEngineBase_hh
+
+// Forward declarations:
+#include <core/engine/MasalaEngineBase.fwd.hh>
+
+// Base headers:
+#include <base/MasalaObject.hh>
+
+namespace core {
+namespace engine {
+
+/// @brief A base class for Masala engines, which perform hard calculations of a given type using
+/// a given method.
+/// @details Subclasses will be defined for kinematic calculations, packing calculations, minimization
+/// calculations, etc.  Each of these in turn will have subclasses for performing these using different
+/// math libraries, optimizers, hardware, etc.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+class MasalaEngineBase : public base::MasalaObject {
+
+public:
+
+	/// @brief Default constructor.
+	MasalaEngineBase() = default;
+
+	/// @brief Copy constructor.
+	MasalaEngineBase( MasalaEngineBase const & ) = default;
+
+	/// @brief Pure virtual destructor.  This class cannot be instantiated; only its
+	/// derived classes can.
+	virtual ~MasalaEngineBase() = default;
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+}; // class MasalaEngineBase
+
+} //namespace engine
+} //namespace core
+
+#endif //Masala_src_core_engine_MasalaEngineBase_hh
