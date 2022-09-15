a9771f65e0a7708dc5d0a2f51a1b4cac642075d1
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-09-15 13:32:23 -0400

    Initial commit of generate_library_api.py.

---
+++ code_templates/generate_library_api.py
@@ -0,0 +1,30 @@
+# MIT License
+#
+# Copyright (c) 2022 Vikram K. Mulligan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+## @file code_templates/generate_library_api.py
+## @brief Used during the build process to produce the auto-generated API layer code.
+## @details Usage:
+##          python3 generate_library_api.py <source library name> <json api definition file>
+## @note The source library name is something like "core".  This Python code would then
+## produce code in "core_api/auto_generated_code/".
+## @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
