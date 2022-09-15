da0139571c986027cf6444d48df48b3e493dee67
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-09-15 03:02:31 -0400

    Forward declaration API template.

---
+++ code_templates/api_templates/MasalaClassAPI.fwd.hh
@@ -0,0 +1,27 @@
+<__COMMENTED_LICENCE__>
+
+<__DOXYGEN_FILE_PATH_AND_FILE_NAME__>
+<__DOXYGEN_BRIEF_DESCRIPTION__>
+<__DOXYGEN_DETAILED_DESCRIPTION__>
+<__DOXYGEN_AUTHOR_AND_EMAIL__>
+/// @note This file is an auto-generated API file.  Do not edit it, but instead
+/// edit the <__SOURCE_CLASS_NAMESPACE_AND_NAME__> class's
+/// get_api_definition() function.
+
+<__CPP_FWD_HEADER_GUARD__>
+
+#include <memory> // For std::shared_ptr and std::weak_ptr.
+
+<__CPP_NAMESPACE__>
+
+    /// @brief Forward declaration.
+    class <__SOURCE_CLASS_API_NAME__>;
+
+    using <__SOURCE_CLASS_API_NAME__>SP = std::shared_ptr< <__SOURCE_CLASS_API_NAME__> >;
+    using <__SOURCE_CLASS_API_NAME__>CSP = std::shared_ptr< <__SOURCE_CLASS_API_NAME__> const >;
+    using <__SOURCE_CLASS_API_NAME__>WP = std::weak_ptr< <__SOURCE_CLASS_API_NAME__> >;
+    using <__SOURCE_CLASS_API_NAME__>CWP = std::weak_ptr< <__SOURCE_CLASS_API_NAME__> const >;
+
+<__CPP_END_NAMESPACE__>
+
+<__CPP_END_FWD_HEADER_GUARD__>
