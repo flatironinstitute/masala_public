e3ea71c5380a0a7e3988dd430d0e5c9b26c94e03
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-09-18 01:58:48 -0400

    Adding base API classes.

---
+++ src/base_api/MasalaObjectAPI.fwd.hh
@@ -0,0 +1,58 @@
+/*
+MIT License
+
+Copyright (c) 2022 Vikram K. Mulligan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/// @file src/base_api/MasalaObject.hh
+/// @brief Forward declarations for a base class for all Masala API derived classes.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Masala_src_base_api_MasalaObjectAPI_fwd_hh
+#define Masala_src_base_api_MasalaObjectAPI_fwd_hh
+
+#include <memory> // For std::shared_ptr
+
+namespace masala {
+namespace base_api {
+
+	class MasalaObjectAPI;
+
+	/// @brief We will use the convention that an class name followed by SP
+	/// represents a std::shared_ptr for objects of that class.
+	using MasalaObjectAPISP = std::shared_ptr< MasalaObjectAPI >;
+
+	/// @brief We will use the convention that an class name followed by CSP
+	/// represents a std::shared_ptr for const objects of that class.
+	using MasalaObjectAPICSP = std::shared_ptr< MasalaObjectAPI const >;
+
+	/// @brief We will use the convention that an class name followed by WP
+	/// represents a std::weak_ptr for objects of that class.
+	using MasalaObjectAPIWP = std::weak_ptr< MasalaObjectAPI >;
+
+	/// @brief We will use the convention that an class name followed by CWP
+	/// represents a std::weak_ptr for const objects of that class.
+	using MasalaObjectAPICWP = std::weak_ptr< MasalaObjectAPI const >;
+
+} // namespace base_api
+} // namesapce masala
+
+#endif //Masala_src_base_api_MasalaObjectAPI_fwd_hh
---
+++ src/base_api/MasalaObjectAPI.hh
@@ -0,0 +1,67 @@
+/*
+MIT License
+
+Copyright (c) 2022 Vikram K. Mulligan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/// @file src/base_api/MasalaObjectAPI.hh
+/// @brief A base class for all Masala derived API classes.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Masala_src_base_api_MasalaObjectAPI_hh
+#define Masala_src_base_api_MasalaObjectAPI_hh
+
+// Forward declarations:
+#include <base_api/MasalaObjectAPI.fwd.hh>
+
+// Base headers:
+#include <base/MasalaObject.hh>
+
+namespace masala {
+namespace base_api {
+
+/// @brief A base class for all Masala derived classes.  This allows the possibility of having a
+/// generic MasalaObject pointer or shared pointer.
+/// @note The MasalaConfigurationManager can access the private member functions (and data) of MasalaObjects.
+/// This allows it to call load_configuration() when first needed.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+class MasalaObjectAPI : public base::MasalaObject {
+
+	friend class masala::base::managers::configuration::MasalaConfigurationManager;
+
+public:
+
+	/// @brief Default constructor.
+	MasalaObjectAPI() = default;
+
+	/// @brief Copy constructor.
+	MasalaObjectAPI( MasalaObjectAPI const & ) = default;
+
+	/// @brief Pure virtual destructor.  This class cannot be instantiated; only its
+	/// derived classes can.
+	virtual ~MasalaObjectAPI() = default;
+
+}; // class MasalaObjectAPI
+
+} // namespace base_api
+} // namespace masala
+
+#endif //Masala_src_base_api_MasalaObjectAPI_hh
