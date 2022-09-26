f05c2f270fd6045a60e71b038bbb6c32514e1e48
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-09-25 22:51:14 -0400

    Initial commit of unit tests.

---
+++ tests/unit/core/pose/PoseUnitTests.cc
@@ -0,0 +1,51 @@
+/*
+MIT License
+
+Copyright (c) 2022 Vikram K. Mulligan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/// @file tests/unit/core/pose/PoseUnitTests.cc
+/// @brief Unit tests for the Pose class.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Unit testing library (Catch2) headers:
+#include <external/catch2/include/catch.hpp>
+
+// Unit headers:
+#include <core/pose/Pose.hh>
+
+namespace masala {
+namespace tests {
+namespace unit {
+namespace core {
+namespace pose {
+
+TEST_CASE( "Instantiate a pose", "[core::pose::Pose][Instantiation]" ) {
+    REQUIRE_NOTHROW([&](){
+        std::make_shared< masala::core::pose::Pose >();
+    }() );
+}
+
+} // namespace pose
+} // namespace core
+} // namespace unit
+} // namespace tests
+} // namespace masala
