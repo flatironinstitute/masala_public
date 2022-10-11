bd290fb369f9b5467be5513a6eaeba9b8f4bc732
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-10-11 19:27:03 -0400

    Initial unit test for just launching threads.

---
+++ tests/unit/base/managers/threads/MasalaThreadManagerTests.cc
@@ -0,0 +1,55 @@
+/*
+MIT License
+
+Copyright (c) 2022 Vikram K. Mulligan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/// @file tests/unit/base/masla/threads/MasalaThreadManagerTests.cc
+/// @brief Unit tests for the Masala thread manager singleton.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Unit testing library (Catch2) headers:
+#include <external/catch2/single_include/catch2/catch.hpp>
+
+// Unit headers:
+#include <base/managers/threads/MasalaThreadManager.hh>
+
+namespace masala {
+namespace tests {
+namespace unit {
+namespace base {
+namespace managers {
+namespace threads {
+
+TEST_CASE( "Launch threads.", "[base::managers::threads::MasalaThreadManager][multi-threading][instantiation]" ) {
+    using namespace masala::base::managers::threads;
+    REQUIRE_NOTHROW([&](){
+        MasalaThreadManagerHandle tm = MasalaThreadManager::get_instance();
+        tm->set_total_threads(8);
+    }() );
+}
+
+} // namespace threads
+} // namespace managers
+} // namespace base
+} // namespace unit
+} // namespace tests
+} // namespace masala
