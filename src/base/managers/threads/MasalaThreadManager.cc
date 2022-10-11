ea06abc215fe86ba3e0bf9d5f21adeae505ae190
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-10-11 19:53:37 -0400

    Revert "Fixing number of threads to launch."
    This reverts commit fd5891299041709f8450d06d001f87c0aa3f06fd.

--- src/base/managers/threads/MasalaThreadManager.cc
+++ src/base/managers/threads/MasalaThreadManager.cc
@@ -312,11 +312,8 @@ MasalaThreadManager::set_total_threads(
     }
 
     std::lock_guard< std::mutex > lock( thread_manager_mutex_ );
-    base::Size const total_before( total_threads_ );
     total_threads_ = actual_desired;
-    if( total_threads_ > total_before ) {
-        thread_pool_->launch_threads_if_needed( total_threads_ - total_before );
-    }
+    thread_pool_->launch_threads_if_needed( total_threads_ );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
