8eb65291f2a82ff7472c47bac1feeb3d2de86e15
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-10-19 18:39:19 -0400

    Adding degree of freedom selector base class.

---
+++ src/core_api/base_classes/selectors/degree_of_freedom_selectors/DegreeOfFreedomSelector.fwd.hh
@@ -0,0 +1,67 @@
+/*
+MIT License
+
+Copyright (c) 2022 Vikram K. Mulligan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/// @file src/core_api/base_classes/selectors/degree_of_freedom_selectors//DegreeOfFreedomSelector.fwd.hh
+/// @brief Forward declarations for a pure virtual base class for DegreeOfFreedomSelectors.
+/// @details DegreeOfFreedomSelectors select degrees of freedom in a pose.
+/// @note Since this class does not implement class_name() or class_namespace()
+/// functions required by the MasalaObject base class, it remains pure virtual.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Masala_src_core_api_base_classes_selectors_degree_of_freedom_selectors_DegreeOfFreedomSelector_fwd_hh
+#define Masala_src_core_api_base_classes_selectors_degree_of_freedom_selectors_DegreeOfFreedomSelector_fwd_hh
+
+#include <memory> // For std::shared_ptr
+
+namespace masala {
+namespace core_api {
+namespace base_classes {
+namespace selectors {
+namespace degree_of_freedom_selectors {
+
+	class DegreeOfFreedomSelector;
+
+	/// @brief We will use the convention that an class name followed by SP
+	/// represents a std::shared_ptr for objects of that class.
+	using DegreeOfFreedomSelectorSP = std::shared_ptr< DegreeOfFreedomSelector >;
+
+	/// @brief We will use the convention that an class name followed by CSP
+	/// represents a std::shared_ptr for const objects of that class.
+	using DegreeOfFreedomSelectorCSP = std::shared_ptr< DegreeOfFreedomSelector const >;
+
+	/// @brief We will use the convention that an class name followed by WP
+	/// represents a std::weak_ptr for objects of that class.
+	using DegreeOfFreedomSelectorWP = std::weak_ptr< DegreeOfFreedomSelector >;
+
+	/// @brief We will use the convention that an class name followed by CWP
+	/// represents a std::weak_ptr for const objects of that class.
+	using DegreeOfFreedomSelectorCWP = std::weak_ptr< DegreeOfFreedomSelector const >;
+
+} // namespace degree_of_freedom_selectors
+} // namespace selectors
+} // namespace base_classes
+} // namespace core_api
+} // namespace masala
+
+#endif // Masala_src_core_api_base_classes_selectors_degree_of_freedom_selectors_DegreeOfFreedomSelector_fwd_hh
