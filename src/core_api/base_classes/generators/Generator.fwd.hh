a5352d8d8d744142fa25478f98f90333c7f47f01
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-11-30 17:48:03 -0500

    Adding Generator base class.

---
+++ src/core_api/base_classes/generators/Generator.fwd.hh
@@ -0,0 +1,60 @@
+/*
+    Masala
+    Copyright (C) 2022 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/core_api/base_classes/generators/Generator.fwd.hh
+/// @brief Forward declarations for a pure virtual base class for Generators.
+/// @details Generators create an instance of something (e.g. a Pose from a data
+/// source or from some input paramaters).
+/// @note Since this class does not implement class_name() or class_namespace()
+/// functions required by the MasalaObject base class, it remains pure virtual.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Masala_src_core_api_base_classes_generators_Generator_fwd_hh
+#define Masala_src_core_api_base_classes_generators_Generator_fwd_hh
+
+#include <memory> // For std::shared_ptr
+
+namespace masala {
+namespace core_api {
+namespace base_classes {
+namespace generators {
+
+	class Generator;
+
+	/// @brief We will use the convention that an class name followed by SP
+	/// represents a std::shared_ptr for objects of that class.
+	using GeneratorSP = std::shared_ptr< Generator >;
+
+	/// @brief We will use the convention that an class name followed by CSP
+	/// represents a std::shared_ptr for const objects of that class.
+	using GeneratorCSP = std::shared_ptr< Generator const >;
+
+	/// @brief We will use the convention that an class name followed by WP
+	/// represents a std::weak_ptr for objects of that class.
+	using GeneratorWP = std::weak_ptr< Generator >;
+
+	/// @brief We will use the convention that an class name followed by CWP
+	/// represents a std::weak_ptr for const objects of that class.
+	using GeneratorCWP = std::weak_ptr< Generator const >;
+
+} // namespace generators
+} // namespace base_classes
+} // namespace core_api
+} // namesapce masala
+
+#endif //Masala_src_core_api_base_classes_generators_Generator_fwd_hh
---
+++ src/core_api/base_classes/generators/description.md
@@ -0,0 +1,5 @@
+# Generators
+
+## Description
+
+Generators produce an object of a given type, often from some input source.  For instance, a PoseFromPDBGenerator converts the contents of a PDB file into a Pose object.  The Standard Masala Plugins library will offer a number of generators.
