0f360360c50294ee9855903c851e9ebe9cf09022
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-12-09 00:34:38 -0500

    Template forward declarations for auto-generated creator classes.

---
+++ code_templates/api_templates/MasalaPluginCreator.fwd.hh
@@ -0,0 +1,24 @@
+<__COMMENTED_LICENCE__>
+
+<__DOXYGEN_CREATOR_FILE_PATH_AND_FWD_FILE_NAME__>
+/// @brief Creators for <__SOURCE_CLASS_NAMESPACE_AND_NAME__> class.
+/// @details Creators are needed to allow the Masala plugin system to use objects
+/// defined in plugin libraries.
+<__DOXYGEN_AUTHOR_AND_EMAIL__>
+
+<__CPP_CREATOR_FWD_HEADER_GUARD__>
+
+#include <base/managers/memory/util.hh> // For MASALA_SHARED_POINTER
+
+<__CPP_NAMESPACE__>
+
+	class <__CREATOR_CLASS_API_NAME__>;
+
+	using <__CREATOR_CLASS_API_NAME__>SP = MASALA_SHARED_POINTER< <__CREATOR_CLASS_API_NAME__> >;
+	using <__CREATOR_CLASS_API_NAME__>CSP = MASALA_SHARED_POINTER< <__CREATOR_CLASS_API_NAME__> const >;
+	using <__CREATOR_CLASS_API_NAME__>WP = MASALA_WEAK_POINTER< <__CREATOR_CLASS_API_NAME__> >;
+	using <__CREATOR_CLASS_API_NAME__>CWP = MASALA_WEAK_POINTER< <__CREATOR_CLASS_API_NAME__> const >;
+
+<__CPP_END_NAMESPACE__>
+
+<__CPP_END_FWD_HEADER_GUARD__>
