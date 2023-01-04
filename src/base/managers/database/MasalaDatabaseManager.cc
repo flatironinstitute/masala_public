045ce7249276c477f8799bf29c00ef8300332ed2
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-01-04 17:39:08 -0500

    Initial commit of the MasalaDatabaseManager.

---
+++ src/base/managers/database/MasalaDatabaseManager.cc
@@ -0,0 +1,69 @@
+/*
+    Masala
+    Copyright (C) 2022 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/base/managers/database/MasalaDatabaseManager.cc
+/// @brief A static singleton for managing data loaded from the Masala database.
+/// @details The core Masala database stores minimal chemical information, such as
+/// element properties.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Project header:
+#include <base/managers/database/MasalaDatabaseManager.hh>
+
+// Base headers:
+#include <base/types.hh>
+#include <base/utility/string/string_manipulation.hh>
+
+// STL headers:
+#include <string>
+#include <iostream>
+
+namespace masala {
+namespace base {
+namespace managers {
+namespace database {
+
+/// @brief Instantiate the static singleton and get a handle to it.
+MasalaDatabaseManagerHandle
+MasalaDatabaseManager::get_instance() {
+    static MasalaDatabaseManager database_manager;
+    return &database_manager;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+/// @brief Get the name of this object.
+/// @details Returns "MasalaDatabaseManager".
+std::string
+MasalaDatabaseManager::class_name() const {
+    return "MasalaDatabaseManager";
+}
+
+/// @brief Get the namespace of this object.
+/// @details Returns "masala::base::managers::database".
+std::string
+MasalaDatabaseManager::class_namespace() const {
+    return "masala::base::managers::database";
+}
+
+} // namespace database
+} // namespace managers
+} // namespace base
+} // namespace masala
---
+++ src/base/managers/database/MasalaDatabaseManager.hh
@@ -0,0 +1,116 @@
+/*
+    Masala
+    Copyright (C) 2022 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/base/managers/database/MasalaDatabaseManager.hh
+/// @brief Headers for a static singleton for managing data loaded
+/// from the Masala database.
+/// @details The core Masala database stores minimal chemical information, such as
+/// element properties.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Masala_src_base_managers_database_MasalaDatabaseManager_hh
+#define Masala_src_base_managers_database_MasalaDatabaseManager_hh
+
+// Parent header:
+#include <base/MasalaObject.hh>
+
+// Forward declarations:
+#include <base/managers/database/MasalaDatabaseManager.fwd.hh>
+
+// Base headers:
+#include <base/types.hh>
+
+// STL headers:
+#include <map>
+#include <mutex>
+#include <thread>
+
+namespace masala {
+namespace base {
+namespace managers {
+namespace database {
+
+
+
+/// @brief A static singleton for managing data loaded from the Masala database.
+/// @details The core Masala database stores minimal chemical information, such as
+/// element properties.
+class MasalaDatabaseManager : public masala::base::MasalaObject {
+
+public:
+
+    /// @brief Instantiate the static singleton and get a handle to it.
+    static MasalaDatabaseManagerHandle get_instance();
+
+private:
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE CONSTRUCTOR
+////////////////////////////////////////////////////////////////////////////////
+
+    /// @brief Private constructor: object can only be instantiated with getInstance().
+    MasalaDatabaseManager() = default;
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC CONSTRUCTORS AND DESTRUCTORS
+////////////////////////////////////////////////////////////////////////////////
+
+    /// @brief No copy constructor.
+    MasalaDatabaseManager( MasalaDatabaseManager const & ) = delete;
+
+    /// @brief No assignment operator.
+    void operator=( MasalaDatabaseManager const & ) = delete;
+
+    /// @brief Default destructor.
+    ~MasalaDatabaseManager() = default;
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+    /// @brief Get the name of this object.
+    /// @details Returns "MasalaDatabaseManager".
+    std::string
+    class_name() const override;
+
+    /// @brief Get the namespace of this object.
+    /// @details Returns "masala::base::managers::database".
+    std::string
+    class_namespace() const override;
+
+private:
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE MEMBER VARIABLES
+////////////////////////////////////////////////////////////////////////////////
+
+    /// @brief A mutex to lock this object.
+    mutable std::mutex masala_database_manager_mutex_;
+
+};
+
+} // namespace database
+} // namespace managers
+} // namespace base
+} // namespace masala
+
+#endif // Masala_src_base_managers_database_MasalaDatabaseManager_hh
