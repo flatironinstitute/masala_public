b7425ab242bb300c98f33857e8fdd86444901998
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-01-10 18:03:58 -0500

    Adding templates for registering plugins.

---
+++ code_templates/api_templates/register_plugins.cc
@@ -0,0 +1,29 @@
+<__COMMENTED_LICENCE__>
+
+<__DOXYGEN_FILE_PATH_AND_CC_FILE_NAME__>
+<__DOXYGEN_BRIEF_DESCRIPTION__>
+<__DOXYGEN_AUTHOR_AND_EMAIL__>
+/// @note This file is an auto-generated API file.  Do not edit it.  Changes will
+/// be lost on next build from source.
+
+// Base headers:
+#include <base/managers/plugin_module/MasalaPluginModuleManager.hh>
+
+// Plugin creator headers:
+<__PLUGIN_CREATOR_HH_FILES_INCLUDES__>
+
+<__CPP_NAMESPACE__>
+
+/// @brief Register all of the plugins in the <__LIBNAME__> library.
+void
+register_<__LIBNAME__>() {
+
+}
+
+/// @brief Unregister all of the plugins in the <__LIBNAME__> library.
+void
+unregister_<__LIBNAME__>() {
+
+}
+
+<__CPP_END_NAMESPACE__>
