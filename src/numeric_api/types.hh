54ad49e223c88784937a7259674168d6050c878d
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-01-21 20:23:55 -0500

    Adding numeric_api types.

---
+++ src/numeric_api/types.hh
@@ -0,0 +1,37 @@
+/*
+    Masala
+    Copyright (C) 2022 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/numeric_api/types.hh
+/// @brief A utility file containing some typedefs.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Masala_src_numeric_api_types_hh
+#define Masala_src_numeric_api_types_hh
+
+#include <numeric/types.hh>
+
+namespace masala {
+namespace numeric_api {
+
+    typedef numeric::Size Size;
+    typedef numeric::Real Real;
+
+} // namespace numeric_api
+} // namespace masala
+
+#endif // Masala_src_numeric_api_types_hh
