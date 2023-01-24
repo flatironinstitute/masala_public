638177a4b47849dd321ac63382b7ede85caac9c8
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-01-24 15:17:49 -0500

    Working on getting the correct parent class.

--- code_templates/generate_library_api.py
+++ code_templates/generate_library_api.py
@@ -326,14 +326,9 @@ def generate_constructor_prototypes(project_name: str, classname: str, jsonfile:
 
 ## @brief Generate the implementations for the constructors based on the JSON description of the API.
 ## @note The classname input should include namespace.
-def generate_constructor_implementations(project_name: str, classname: str, jsonfile: json, tabchar: str, additional_includes: list, is_lightweight : bool, is_plugin_class : bool ) -> str :
+def generate_constructor_implementations(project_name: str, api_base_class : str, classname: str, jsonfile: json, tabchar: str, additional_includes: list, is_lightweight : bool, is_plugin_class : bool ) -> str :
     outstring = ""
     first = True
-
-    if is_plugin_class == True :
-        api_base_class = "masala::base::managers::plugin_module::MasalaPluginAPI"
-    else :
-        api_base_class = "masala::base::MasalaObjectAPI"
 
     for constructor in jsonfile["Elements"][classname]["Constructors"]["Constructor_APIs"] :
         #print(constructor)
@@ -865,6 +860,20 @@ def prepare_forward_declarations( libraryname : str, classname : str, namespace 
         filehandle.write(fwdfile)
     print( "\tWrote \"" + fname + "\"."  )
 
+## @brief Figure out the parent class for this API class, and the include file that defines the parent class.
+## @details If the parent class of the inner object has an API, use the API class for that object as the parent.  Otherwise,
+## use MasalaPluginAPI (if it is a plug-in class) or MasalaObjectAPI (if it is not).
+def get_api_class_include_and_classname( project_name : str, libraryname : str, classname : str, namespace : str, is_plugin_class : str ) -> tuple( str, str ) :
+    TODO TODO TODO
+
+    ## If we reach here, there's no parent class with an API.
+    if is_plugin_class == True :
+        return ( "#include <base/managers/plugin_module/MasalaPluginAPI.hh>", \
+            "masala::base::managers::plugin_module::MasalaPluginAPI")
+    else :
+        return ( "#include <base/MasalaObjectAPI.hh>", \
+            "masala::base::MasalaObjectAPI" )
+
 ## @brief Auto-generate the header file (***.hh) for the class.
 def prepare_header_file( project_name: str, libraryname : str, classname : str, namespace : list, dirname : str, hhfile_template : str, licence : str, jsonfile : json, tabchar : str, is_plugin_class : bool ) :
     apiclassname = classname + "_API"
@@ -878,12 +887,7 @@ def prepare_header_file( project_name: str, libraryname : str, classname : str, 
             header_guard_string += namespace[i] + "_"
     header_guard_string += apiclassname + "_hh"
 
-    if is_plugin_class == True :
-        api_base_class_include = "#include <base/managers/plugin_module/MasalaPluginAPI.hh>"
-        api_base_class = "masala::base::managers::plugin_module::MasalaPluginAPI"
-    else :
-        api_base_class_include = "#include <base/MasalaObjectAPI.hh>"
-        api_base_class = "masala::base::MasalaObjectAPI"
+    api_base_class_include, api_base_class = get_api_class_include_and_classname( project_name, libraryname, classname, namespace, is_plugin_class )
 
     dirname_short = dirname.replace("src/", "")
     namespace_and_source_class = original_class_namespace_string + "::" + classname
@@ -934,10 +938,7 @@ def prepare_cc_file( project_name: str, libraryname : str, classname : str, name
     dirname_short = dirname.replace("src/", "")
     namespace_and_source_class = original_class_namespace_string + "::" + classname
 
-    if is_plugin_class == True :
-        api_base_class = "masala::base::managers::plugin_module::MasalaPluginAPI"
-    else :
-        api_base_class = "masala::base::MasalaObjectAPI"
+    api_base_class_include, api_base_class = get_api_class_include_and_classname( project_name, libraryname, classname, namespace, is_plugin_class )
 
     additional_includes = []
 
@@ -957,7 +958,7 @@ def prepare_cc_file( project_name: str, libraryname : str, classname : str, name
         .replace( "<__SOURCE_CLASS_API_NAMESPACE__>", generate_cpp_namespace_singleline( namespace ) ) \
         .replace( "<__INCLUDE_FILE_PATH_AND_HH_FILE_NAME__>", "#include <" + dirname_short + apiclassname + ".hh>" ) \
         .replace( "<__INCLUDE_SOURCE_FILE_PATH_AND_HH_FILE_NAME__>", "#include <" + generate_source_class_filename( classname, namespace, ".hh" ) + ">" ) \
-        .replace( "<__CPP_CONSTRUCTOR_IMPLEMENTATIONS__>", generate_constructor_implementations(project_name, namespace_and_source_class, jsonfile, tabchar, additional_includes, is_lightweight, is_plugin_class=is_plugin_class) ) \
+        .replace( "<__CPP_CONSTRUCTOR_IMPLEMENTATIONS__>", generate_constructor_implementations(project_name, api_base_class, namespace_and_source_class, jsonfile, tabchar, additional_includes, is_lightweight, is_plugin_class=is_plugin_class) ) \
         .replace( "<__CPP_SETTER_IMPLEMENTATIONS__>", generate_function_implementations(project_name, namespace_and_source_class, jsonfile, tabchar, "SETTER", additional_includes, is_lightweight) ) \
         .replace( "<__CPP_GETTER_IMPLEMENTATIONS__>", generate_function_implementations(project_name, namespace_and_source_class, jsonfile, tabchar, "GETTER", additional_includes, is_lightweight) ) \
         .replace( "<__CPP_WORK_FUNCTION_IMPLEMENTATIONS__>", generate_function_implementations(project_name, namespace_and_source_class, jsonfile, tabchar, "WORKFXN", additional_includes, is_lightweight) ) \
