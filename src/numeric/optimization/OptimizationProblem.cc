1109ce8a411b39f12c55d15dbf4529782d4b0f25
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-01-24 12:13:54 -0500

    Need deep clone.

--- src/numeric/optimization/OptimizationProblem.cc
+++ src/numeric/optimization/OptimizationProblem.cc
@@ -61,6 +61,14 @@ OptimizationProblem::operator=(
     return *this;
 }
 
+/// @brief Make a fully independent copy of this object.
+OptimizationProblemSP
+OptimizationProblem::deep_clone() const {
+    OptimizationProblemSP new_object( masala::make_shared< OptimizationProblem >( *this ) );
+    new_object->make_independent();
+    return new_object;
+}
+
 /// @brief Ensure that all data are unique and not shared (i.e. everytihng is deep-cloned.)
 void
 OptimizationProblem::make_independent() {
--- src/numeric/optimization/OptimizationProblem.hh
+++ src/numeric/optimization/OptimizationProblem.hh
@@ -66,6 +66,10 @@ public:
 	/// @brief Destructor.
 	~OptimizationProblem() override = default;
 
+	/// @brief Make a fully independent copy of this object.
+	OptimizationProblemSP
+	deep_clone() const;
+
 	/// @brief Ensure that all data are unique and not shared
 	/// (i.e. everytihng is deep-cloned.)
 	void make_independent();
