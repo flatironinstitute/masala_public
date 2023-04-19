1f612107c737cc1fd3c798a02eaf106870f2db58
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-04-19 01:09:38 -0400

    Initial commit of ChoiceFeature class.

---
+++ src/numeric/optimization/cost_function_network/cost_function/feature_based/ChoiceFeature.cc
@@ -0,0 +1,111 @@
+/*
+    Masala
+    Copyright (C) 2022 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/numeric/optimization/cost_function_network/cost_function/feature_based/ChoiceFeature.cc
+/// @brief Implementation for a class for ChoiceFeatures.
+/// @details ChoiceFeatures are objects attached to node choices, which can form connections across
+/// choices at different nodes.  Each feature has a minimum and maximum number of connections that
+/// it must make to be satisfied.
+/// @note This class is a lightweight class that offers no thread safety.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Unit header:
+#include <numeric/optimization/cost_function_network/cost_function/feature_based/ChoiceFeature.hh>
+
+// STL headers:
+#include <vector>
+#include <string>
+
+// Base headers:
+#include <base/error/ErrorHandling.hh>
+
+namespace masala {
+namespace numeric {
+namespace optimization {
+namespace cost_function_network {
+namespace cost_function {
+namespace feature_based {
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTRUCTION AND DESTRUCTION
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+/// @brief Get the category or categories for this plugin class.  Default for all
+/// optimization problems; may be overridden by derived classes.
+/// @returns { { "ChoiceFeature" } }
+/// @note Categories are hierarchical (e.g. Selector->AtomSelector->AnnotatedRegionSelector,
+/// stored as { {"Selector", "AtomSelector", "AnnotatedRegionSelector"} }). A plugin can be
+/// in more than one hierarchical category (in which case there would be more than one
+/// entry in the outer vector), but must be in at least one.  The first one is used as
+/// the primary key.
+std::vector< std::vector< std::string > >
+ChoiceFeature::get_categories() const {
+	return std::vector< std::vector< std::string > >{ { "ChoiceFeature" } };
+}
+
+/// @brief Get the keywords for this plugin class.  Default for all
+/// optimization problems; may be overridden by derived classes.
+/// @returns { "optimization_problem", "cost_function", "numeric", "choice_feature" }
+std::vector< std::string >
+ChoiceFeature::get_keywords() const {
+	std::vector< std::string > outvec( CostFunction::get_keywords() );
+    outvec.push_back( "choice_feature" );
+    return outvec;
+}
+
+/// @brief This returns the class name ("ChoiceFeature").
+std::string
+ChoiceFeature::class_name() const {
+    return "ChoiceFeature";
+}
+
+/// @brief This returns the class namespace ("masala::numeric::optimization::cost_function_network::cost_function::feature_based").
+std::string
+ChoiceFeature::class_namespace() const {
+    return "masala::numeric::optimization::cost_function_network::cost_function::feature_based";
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// GETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+// SETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+// WORK FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC INTERFACE DEFINITION
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace feature_based
+} // namespace cost_function
+} // namespace cost_function_network
+} // namespace optimization
+} // namespace numeric
+} // namespace masala
---
+++ src/numeric/optimization/cost_function_network/cost_function/feature_based/ChoiceFeature.hh
@@ -0,0 +1,158 @@
+/*
+    Masala
+    Copyright (C) 2022 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/numeric/optimization/cost_function_network/cost_function/feature_based/ChoiceFeature.hh
+/// @brief Headers for a class for ChoiceFeatures.
+/// @details ChoiceFeatures are objects attached to node choices, which can form connections across
+/// choices at different nodes.  Each feature has a minimum and maximum number of connections that
+/// it must make to be satisfied.
+/// @note This class is a lightweight class that offers no thread safety.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Masala_src_numeric_optimization_cost_function_network_cost_function_feature_based_ChoiceFeature_hh
+#define Masala_src_numeric_optimization_cost_function_network_cost_function_feature_based_ChoiceFeature_hh
+
+// Forward declarations:
+#include <numeric/optimization/cost_function_network/cost_function/feature_based/ChoiceFeature.fwd.hh>
+
+// Parent header:
+#include <base/managers/plugin_module/MasalaPlugin.hh>
+
+// Numeric headers:
+
+// Base headers:
+#include <base/types.hh>
+
+// STL headers:
+
+
+namespace masala {
+namespace numeric {
+namespace optimization {
+namespace cost_function_network {
+namespace cost_function {
+namespace feature_based {
+
+/// @brief A pure virtual base class for ChoiceFeatures.
+/// @brief A class for ChoiceFeatures.
+/// @details ChoiceFeatures are objects attached to node choices, which can form connections across
+/// choices at different nodes.  Each feature has a minimum and maximum number of connections that
+/// it must make to be satisfied.
+/// @note This class is a lightweight class that offers no thread safety.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+class ChoiceFeature : public masala::base::managers::plugin_module::MasalaPlugin {
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTRUCTION AND DESTRUCTION
+////////////////////////////////////////////////////////////////////////////////
+
+	/// @brief Default constructor.
+	ChoiceFeature() = default;
+
+	/// @brief Copy constructor.
+	ChoiceFeature(
+		ChoiceFeature const &
+	) = default;
+
+	// @brief Assignment operator.
+	ChoiceFeature &
+	operator=( ChoiceFeature const & ) = default;
+
+	/// @brief Destructor.
+	~ChoiceFeature() override = default;
+
+	/// @brief Copy this object and return a shared pointer to the copy.
+	CostFunctionSP clone() const override;
+
+	/// @brief Ensure that this object is fully independent.
+	void make_independent() override;
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+	/// @brief Get the category or categories for this plugin class.  Default for all
+	/// cost functions; may be overridden by derived classes.
+	/// @returns { { "ChoiceFeature" } }
+	/// @note Categories are hierarchical (e.g. Selector->AtomSelector->AnnotatedRegionSelector,
+	/// stored as { {"Selector", "AtomSelector", "AnnotatedRegionSelector"} }). A plugin can be
+	/// in more than one hierarchical category (in which case there would be more than one
+	/// entry in the outer vector), but must be in at least one.  The first one is used as
+	/// the primary key.
+	std::vector< std::vector< std::string > >
+	get_categories() const override;
+
+	/// @brief Get the keywords for this plugin class.
+	/// @returns { "optimization_problem", "cost_function", "numeric", "choice_feature" }
+	std::vector< std::string >
+	get_keywords() const override;
+
+	/// @brief This returns the class name ("ChoiceFeature").
+	std::string class_name() const override;
+
+	/// @brief This returns the class namespace ("masala::numeric::optimization::cost_function_network::cost_function::feature_based").
+	std::string class_namespace() const override;
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// GETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// SETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// WORK FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC INTERFACE DEFINITION
+////////////////////////////////////////////////////////////////////////////////
+
+private:
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE VARIABLES
+////////////////////////////////////////////////////////////////////////////////
+
+
+}; // class ChoiceFeature
+
+} // namespace feature_based
+} // namespace cost_function
+} // namespace cost_function_network
+} // namespace optimization
+} // namespace numeric
+} // namespace masala
+
+#endif // Masala_src_numeric_optimization_cost_function_network_cost_function_feature_based_ChoiceFeature_hh
