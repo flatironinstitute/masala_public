8ea46b2a102d5cca069f4011868fe0d4eadae623
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-04-29 18:50:49 -0400

    Add ChoiceFeature to API classes.

--- src/numeric/api/generate_api_classes.cc
+++ src/numeric/api/generate_api_classes.cc
@@ -39,6 +39,7 @@
 #include <numeric/optimization/cost_function_network/cost_function/CostFunction.hh>
 #include <numeric/optimization/cost_function_network/cost_function/FunctionOfIntegerPenaltySumCostFunction.hh>
 #include <numeric/optimization/cost_function_network/cost_function/SquareOfChoicePenaltySumCostFunction.hh>
+#include <numeric/optimization/cost_function_network/cost_function/feature_based/ChoiceFeature.hh>
 
 namespace masala {
 namespace numeric {
@@ -66,6 +67,7 @@ namespace api {
         outvec.emplace_back( masala::make_shared< masala::numeric::optimization::cost_function_network::cost_function::CostFunction >() );
         outvec.emplace_back( masala::make_shared< masala::numeric::optimization::cost_function_network::cost_function::FunctionOfIntegerPenaltySumCostFunction >() );
         outvec.emplace_back( masala::make_shared< masala::numeric::optimization::cost_function_network::cost_function::SquareOfChoicePenaltySumCostFunction >() );
+        outvec.emplace_back( masala::make_shared< masala::numeric::optimization::cost_function_network::cost_function::feature_based::ChoiceFeature >() );
         // ADD MORE ENTRIES HERE
 
         return outvec;
