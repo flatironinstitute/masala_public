b5d06867ea15fdaeff4eaadbbcdd1b2325dd961f
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2024-04-17 23:02:30 -0400

    Initial commit of ScoringTerm base class.

---
+++ src/core/scoring/ScoringTerm.cc
@@ -0,0 +1,46 @@
+/*
+    Masala
+    Copyright (C) 2024 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/core/scoring/ScoringTerm.cc
+/// @brief A base class for all Masala scoring terms.
+/// @details Masala scoring terms are components of Masala scoring functions.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Class headers:
+#include <core/scoring/ScoringTerm.hh>
+
+// Base headers:
+
+// STL headers:
+
+namespace masala {
+namespace core {
+namespace scoring {
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// PROTECTED MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+
+} // namespace scoring
+} // namespace core
+} // namespace masala
