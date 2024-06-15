47637d6de79b2dfdd99a2215dbe47a7587ff4311
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2024-06-14 22:02:41 -0400

    Typo.

--- src/numeric/optimization/cost_function_network/CostFunctionNetworkOptimizationProblem.cc
+++ src/numeric/optimization/cost_function_network/CostFunctionNetworkOptimizationProblem.cc
@@ -519,7 +519,7 @@ CostFunctionNetworkOptimizationProblem::get_api_definition() {
 		);
 		api_def->add_setter(
 			masala::make_shared< setter::MasalaObjectAPISetterDefinition_OneInput< std::vector< masala::base::Size > const & > >(
-				"add_candidate_solution", "Add a candidate solution.  this may or may not be used as a starting point by a given solver.  This function "
+				"add_candidate_solution", "Add a candidate solution.  This may or may not be used as a starting point by a given solver.  This function "
 				"locks the problem mutex.  It throws if the problem has already been finalized.",
 				"candidate_solution_in", "The input candidate solution.  This should be a vector of zero-based choice indices, with one "
 				"index for each variable node in the problem.", false, false,
