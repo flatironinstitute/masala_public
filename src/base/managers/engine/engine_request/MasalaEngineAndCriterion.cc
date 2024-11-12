29c047dae9514b303320214ba27f392be0dd96dc
Vikram Mulligan <vmulligan@MacBookPro.fios-router.home>
2024-11-12 00:03:52 -0500

    Adding an AND criterion.

---
+++ src/base/managers/engine/engine_request/MasalaEngineAndCriterion.cc
@@ -0,0 +1,109 @@
+/*
+    Masala
+    Copyright (C) 2024 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/base/managers/engine/engine_request/MasalaEngineAndCriterion.cc
+/// @brief A class for imposing the condition that a particular engine be in (or not in) a particular category.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Headers:
+#include <base/managers/engine/engine_request/MasalaEngineAndCriterion.hh>
+
+// Base headers:
+#include <base/utility/container/container_util.tmpl.hh>
+#include <base/error/ErrorHandling.hh>
+#include <base/types.hh>
+#include <base/managers/engine/MasalaEngineCreator.hh>
+// #include <base/managers/tracer/MasalaTracerManager.hh> // DELETE ME
+
+// STL headers:
+#include <vector>
+#include <string>
+
+namespace masala {
+namespace base {
+namespace managers {
+namespace engine {
+namespace engine_request {
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+/// @brief Get the name of this class.
+/// @returns "MasalaEngineAndCriterion".
+std::string
+MasalaEngineAndCriterion::class_name() const {
+    return class_name_static();
+}
+
+/// @brief Get the namespace of this class.
+/// @returns "masala::base::managers::engine::engine_request".
+std::string
+MasalaEngineAndCriterion::class_namespace() const {
+    return class_namespace_static();
+}
+
+/// @brief Get the name of this class.
+/// @returns "MasalaEngineAndCriterion".
+/*static*/
+std::string
+MasalaEngineAndCriterion::class_name_static() {
+    return "MasalaEngineAndCriterion";
+}
+
+/// @brief Get the namespace of this class.
+/// @returns "masala::base::managers::engine::engine_request".
+/*static*/
+std::string
+MasalaEngineAndCriterion::class_namespace_static() {
+    return "masala::base::managers::engine::engine_request";
+}
+
+/// @brief Determine whether a particular engine is compatible with this criterion.
+/// @returns True if it is compatible, false otherwise.
+bool
+MasalaEngineAndCriterion::engine_is_compatible_with_criterion(
+    masala::base::managers::engine::MasalaEngineCreator const & creator
+) const {
+	for( auto const & criterion : and_criteria_ ) {
+		if( !(criterion->engine_is_compatible_with_criterion( creator )) ) {
+			return false;
+		}
+	}
+    return true;
+}
+
+/// @brief Add a MasalaEngineRequestCriterion to the set of criteria that all must be true for this
+/// criterion to pass.
+/// @details The input criterion is used directly, not cloned.
+void
+MasalaEngineAndCriterion::add_criterion(
+	MasalaEngineRequestCriterionCSP const & criterion_in
+) {
+	and_criteria_.push_back( criterion_in );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace engine_request
+} // namespace engine
+} // namespace managers
+} // namespace base
+} // namespace masala
