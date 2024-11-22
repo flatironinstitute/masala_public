965483d0a53fa8b4dabf33c993f0b7838ef0f971
Vikram Mulligan <vmulligan@flatironinstitute.org>
2024-11-22 00:33:23 -0500

    Updating OptimizationProblem base class.

--- src/numeric/optimization/OptimizationProblem.cc
+++ src/numeric/optimization/OptimizationProblem.cc
@@ -50,26 +50,15 @@ namespace optimization {
 /// @brief Copy constructor.
 /// @details Must be explicitly defined due to mutex.
 OptimizationProblem::OptimizationProblem(
-    OptimizationProblem const & src
+	OptimizationProblem const & src
 ) :
-    masala::base::managers::engine::MasalaDataRepresentation(src)
+	masala::base::managers::engine::MasalaDataRepresentation()
 {
-    std::lock_guard< std::mutex > lock( src.problem_mutex_ );
-    finalized_.store(false);
+	std::lock( data_representation_mutex(), src.data_representation_mutex() );
+	std::lock_guard< std::mutex > lockthis( data_representation_mutex(), std::adopt_lock );
+	std::lock_guard< std::mutex > lockthat( src.data_representation_mutex(), std::adopt_lock );
+	finalized_.store(false);
 	protected_assign(src);
-}
-
-/// @brief Assignment operator.
-OptimizationProblem &
-OptimizationProblem::operator=(
-    OptimizationProblem const & src
-) {
-    masala::base::managers::engine::MasalaDataRepresentation::operator=(src);
-	std::lock( problem_mutex_, src.problem_mutex_ );
-	std::lock_guard< std::mutex > lock( problem_mutex_, std::adopt_lock );
-	std::lock_guard< std::mutex > lock2( src.problem_mutex_, std::adopt_lock );
-	protected_assign(src);
-    return *this;
 }
 
 /// @brief Make a copy of this object, and return a shared pointer to the copy.
@@ -82,16 +71,9 @@ OptimizationProblem::clone() const {
 /// @brief Make a fully independent copy of this object.
 OptimizationProblemSP
 OptimizationProblem::deep_clone() const {
-    OptimizationProblemSP new_object( clone() );
-    new_object->make_independent();
-    return new_object;
-}
-
-/// @brief Ensure that all data are unique and not shared (i.e. everytihng is deep-cloned.)
-void
-OptimizationProblem::make_independent() {
-    std::lock_guard< std::mutex > lock( problem_mutex_ );
-    protected_make_independent();
+	OptimizationProblemSP new_object( clone() );
+	new_object->make_independent();
+	return new_object;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -130,7 +112,7 @@ OptimizationProblem::get_keywords() const {
 /// @returns {{ "OptimizationProblem" }}
 std::vector< std::vector< std::string > >
 OptimizationProblem::get_data_representation_categories() const {
-    return std::vector< std::vector< std::string > >{{"OptimizationProblem"}};
+	return std::vector< std::vector< std::string > >{{"OptimizationProblem"}};
 }
 
 /// @brief Get the keywords that this data representation plugin has.
@@ -139,7 +121,7 @@ OptimizationProblem::get_data_representation_categories() const {
 /// @returns { "optimization_problem", "numeric" }
 std::vector< std::string >
 OptimizationProblem::get_data_representation_keywords() const {
-    return std::vector< std::string > {
+	return std::vector< std::string > {
 		"optimization_problem",
 		"numeric"
 	};
@@ -154,28 +136,21 @@ OptimizationProblem::get_data_representation_keywords() const {
 /// @returns An empty list.
 std::vector< std::string >
 OptimizationProblem::get_compatible_masala_engines() const {
-    return std::vector< std::string >{};
+	return std::vector< std::string >{};
 }
 
 /// @brief Get the class name.
 /// @returns "OptimizationProblem".
 std::string
 OptimizationProblem::class_name() const {
-    return "OptimizationProblem";
+	return "OptimizationProblem";
 }
 
 /// @brief Get the class namespace.
 /// @returns "masala::numeric::optimization".
 std::string
 OptimizationProblem::class_namespace() const {
-    return "masala::numeric::optimization";
-}
-
-/// @brief Reset all data in this object.
-void
-OptimizationProblem::reset() {
-    std::lock_guard< std::mutex > lock( problem_mutex_ );
-    protected_reset();
+	return "masala::numeric::optimization";
 }
 
 /// @brief Finalize this problem: indicate that all problem setup is complete, and
@@ -183,16 +158,16 @@ OptimizationProblem::reset() {
 /// @details Derived classes should probably override this.
 void
 OptimizationProblem::finalize() {
-    std::lock_guard< std::mutex > lock( problem_mutex_ );
-    protected_finalize();
+	std::lock_guard< std::mutex > lock( data_representation_mutex() );
+	protected_finalize();
 }
 
 /// @brief Has this problem been finalized?
 /// @details Locks mutex for check.
 bool
 OptimizationProblem::finalized() const {
-    std::lock_guard< std::mutex > lock( problem_mutex_ );
-    return protected_finalized();
+	std::lock_guard< std::mutex > lock( data_representation_mutex() );
+	return protected_finalized();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -202,71 +177,71 @@ OptimizationProblem::finalized() const {
 /// @brief Get a description of the API for the OptimizationProblem class.
 masala::base::api::MasalaObjectAPIDefinitionCWP
 OptimizationProblem::get_api_definition() {
-    using namespace masala::base::api;
+	using namespace masala::base::api;
 
 
-    std::lock_guard< std::mutex > lock( problem_mutex_ );
+	std::lock_guard< std::mutex > lock( data_representation_mutex() );
 
-    if( api_definition_ == nullptr ) {
+	if( api_definition_ == nullptr ) {
 
-        MasalaObjectAPIDefinitionSP api_def(
-            masala::make_shared< MasalaObjectAPIDefinition >(
-                *this,
-                "The OptimizationProblem class defines a numerical optimization problem.  This is "
-                "the problem reduced to numbers, with no chemical classes or concepts included.  Typically, "
-                "one would not want to instantiate an abstract OptimizationProblem.  Instead, one would "
-                "usually use a particular sub-class defining a particular type of optimization problem, "
-                "such as a CostFunctionNetworkOptimizationProblem.",
-                false, false
-            )
-        );
+		MasalaObjectAPIDefinitionSP api_def(
+			masala::make_shared< MasalaObjectAPIDefinition >(
+				*this,
+				"The OptimizationProblem class defines a numerical optimization problem.  This is "
+				"the problem reduced to numbers, with no chemical classes or concepts included.  Typically, "
+				"one would not want to instantiate an abstract OptimizationProblem.  Instead, one would "
+				"usually use a particular sub-class defining a particular type of optimization problem, "
+				"such as a CostFunctionNetworkOptimizationProblem.",
+				false, false
+			)
+		);
 
-        // Constructors:
-        ADD_PUBLIC_CONSTRUCTOR_DEFINITIONS( OptimizationProblem, api_def );
+		// Constructors:
+		ADD_PUBLIC_CONSTRUCTOR_DEFINITIONS( OptimizationProblem, api_def );
 
-        // Getters:
-        api_def->add_getter(
-            masala::make_shared< getter::MasalaObjectAPIGetterDefinition_ZeroInput< bool > >(
-                "finalized", "Has this problem description been finalized?  That is, is the problem setup "
-                "complete and the object locked to now be read-only?",
-                "finalized", "True if the object has been finalized, false otherwise.",
-                false, false,
-                std::bind( &OptimizationProblem::finalized, this )
-            )
-        );
+		// Getters:
+		api_def->add_getter(
+			masala::make_shared< getter::MasalaObjectAPIGetterDefinition_ZeroInput< bool > >(
+				"finalized", "Has this problem description been finalized?  That is, is the problem setup "
+				"complete and the object locked to now be read-only?",
+				"finalized", "True if the object has been finalized, false otherwise.",
+				false, false,
+				std::bind( &OptimizationProblem::finalized, this )
+			)
+		);
 
-        // Setters:
-        api_def->add_setter(
-            masala::make_shared< setter::MasalaObjectAPISetterDefinition_ZeroInput >(
-                "finalize", "Finalize this object completely -- i.e. indicate that all problem setup is complete, and "
-                "the object should now be read-only.  May be overridden by derived classes.",
-                true, false, std::bind( &OptimizationProblem::finalize, this )
-            )
-        );
-        api_def->add_setter(
-            masala::make_shared< setter::MasalaObjectAPISetterDefinition_ZeroInput >(
-                "reset", "Reset this object completely.  (Resets finalization state.)",
-                true, false, std::bind( &OptimizationProblem::reset, this )
-            )
-        );
+		// Setters:
+		api_def->add_setter(
+			masala::make_shared< setter::MasalaObjectAPISetterDefinition_ZeroInput >(
+				"finalize", "Finalize this object completely -- i.e. indicate that all problem setup is complete, and "
+				"the object should now be read-only.  May be overridden by derived classes.",
+				true, false, std::bind( &OptimizationProblem::finalize, this )
+			)
+		);
+		api_def->add_setter(
+			masala::make_shared< setter::MasalaObjectAPISetterDefinition_ZeroInput >(
+				"reset", "Reset this object completely.  (Resets finalization state.)",
+				true, false, std::bind( &OptimizationProblem::reset, this )
+			)
+		);
 
-        // Work functions:
-        api_def->add_work_function(
-            masala::make_shared< work_function::MasalaObjectAPIWorkFunctionDefinition_ZeroInput< OptimizationSolutionsSP > >(
-                "create_solutions_container", "Create a solutions container for this type of optimization problem.  "
+		// Work functions:
+		api_def->add_work_function(
+			masala::make_shared< work_function::MasalaObjectAPIWorkFunctionDefinition_ZeroInput< OptimizationSolutionsSP > >(
+				"create_solutions_container", "Create a solutions container for this type of optimization problem.  "
 				"Base class implementation creates a generic OptimizationSolutions container.  Derived classes may "
 				"override this to create specialized solutions containers.",
 				true, false, true, false,
 				"solutions_container", "An OptimizationSolutions object (or instance of a derived class thereof) for holding "
 				"solutions to this optimization problem.",
 				std::bind( &OptimizationProblem::create_solutions_container, this )
-            )
-        );
+			)
+		);
 
-        api_definition_ = api_def; //Make const.
-    }
+		api_definition_ = api_def; //Make const.
+	}
 
-    return api_definition_;
+	return api_definition_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -278,55 +253,52 @@ OptimizationProblem::get_api_definition() {
 /// classes may override this to create specialized solutions containers.
 OptimizationSolutionsSP
 OptimizationProblem::create_solutions_container() const {
-    return masala::make_shared< OptimizationSolutions >();
+	return masala::make_shared< OptimizationSolutions >();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // PROTECTED FUNCTIONS
 ////////////////////////////////////////////////////////////////////////////////
 
-/// @brief Allow derived classes to access the mutex for this object.
-/// @note The mutex is mutable, and can be locked from a const function.
-std::mutex &
-OptimizationProblem::problem_mutex() const {
-    return problem_mutex_;
-}
-
 /// @brief Allow derived classes to access the API definition.
 /// @note Could be nullptr.
 masala::base::api::MasalaObjectAPIDefinitionCSP &
 OptimizationProblem::api_definition() {
-    return api_definition_;
+	return api_definition_;
 }
 
 /// @brief Inner workings of finalize function.  Should be called with locked mutex.
 /// Base class protected_finalize() sets finalized_ to true.
 void
 OptimizationProblem::protected_finalize() {
-    CHECK_OR_THROW_FOR_CLASS( !finalized_, "protected_finalize",
-        "This function was called on a " + class_name() + "problem definition that was already finalized!"
-    );
-    finalized_ = true;
+	CHECK_OR_THROW_FOR_CLASS( !finalized_, "protected_finalize",
+		"This function was called on a " + class_name() + "problem definition that was already finalized!"
+	);
+	finalized_ = true;
 }
-
 
 /// @brief Inner workings of assignment operator.  Should be called with locked mutex.
 /// Should be implemented by derived classes, which shoudl call base class function.
 void
 OptimizationProblem::protected_assign(
-	OptimizationProblem const & /*src*/
+	Parent const & src
 ) {
+	OptimizationProblem const * src_cast_ptr( dynamic_cast< OptimizationProblem const * >( &src ) );
+	CHECK_OR_THROW_FOR_CLASS( src_cast_ptr != nullptr, "protected_assign", "The " + src.class_name() + " object passed to this "
+		"function was not an OptimizationProblem object."
+	);
 	CHECK_OR_THROW_FOR_CLASS( !finalized_, "protected_assign",
-        "This function was called on a " + class_name() + " problem definition that was already finalized!"
-    );
-	// GNDN.
+		"This function was called on a " + class_name() + " problem definition that was already finalized!"
+	);
+	Parent::protected_assign( src );
 }
 
 /// @brief Reset all data in this object.
 /// @details Sets state to not finalized.  Mutex must be locked before calling this.
 void
 OptimizationProblem::protected_reset() {
-    finalized_.store(false);
+	finalized_.store(false);
+	//Parent::protected_reset();
 }
 
 /// @brief Make this object independent.
@@ -335,6 +307,23 @@ OptimizationProblem::protected_reset() {
 void
 OptimizationProblem::protected_make_independent() {
 	api_definition_ = nullptr;
+	//Parent::protected_make_independent();
+}
+
+/// @brief Is this data representation empty?
+/// @details Must be implemented by derived classes.  Should return its value && the parent class protected_empty().  Performs no mutex-locking.
+/// @returns True if no data have been loaded into this data representation, false otherwise.
+/// @note This does not report on whether the data representation has been configured; only whether it has been loaded with data.
+bool
+OptimizationProblem::protected_empty() const {
+	return true;
+}
+
+/// @brief Remove the data loaded in this object.  Note that this does not result in the configuration being discarded.
+/// @details Must be implemented by derived classes, and should call parent class protected_clear().  Performs no mutex-locking.
+void
+OptimizationProblem::protected_clear() {
+	// GNDN.
 }
 
 } // namespace optimization
