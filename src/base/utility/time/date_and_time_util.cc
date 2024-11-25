d32248bef8133cae5dfb9ffa906599e3238e1a24
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2024-11-25 16:41:23 -0500

    Initial commit of date_and_time_util.hh/cc.

---
+++ src/base/utility/time/date_and_time_util.cc
@@ -0,0 +1,36 @@
+/*
+    Masala
+    Copyright (C) 2024 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/base/utility/time/date_and_time_util.cc
+/// @brief Functions for getting times and dates.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// STL headers
+#include <vector>
+#include <string>
+
+namespace masala {
+namespace base {
+namespace utility {
+namespace time {
+
+
+} // namespace time
+} // namespace utility
+} // namespace base
+} // namespace masala
---
+++ src/base/utility/time/date_and_time_util.hh
@@ -0,0 +1,41 @@
+/*
+    Masala
+    Copyright (C) 2024 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/base/utility/time/date_and_time_util.hh
+/// @brief Functions for getting times and dates.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Masala_src_base_utility_time_date_and_time_util_hh
+#define Masala_src_base_utility_time_date_and_time_util_hh
+
+// STL headers
+#include <vector>
+#include <string>
+
+namespace masala {
+namespace base {
+namespace utility {
+namespace time {
+
+
+} // namespace time
+} // namespace utility
+} // namespace base
+} // namespace masala
+
+#endif //Masala_src_base_utility_time_date_and_time_util_hh
