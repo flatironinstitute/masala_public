8222720ba0d30bfdee200bb2e5d1775ed8e9ff53
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2024-12-04 23:27:49 -0500

    Hmm.  Let's try this.

--- src/numeric/utility/cxx_17_compatibility_util.hh
+++ src/numeric/utility/cxx_17_compatibility_util.hh
@@ -45,7 +45,7 @@ reduce(
 #if defined MASALA_USE_TRANSFORM_REDUCE && defined __cplusplus && __cplusplus >= 201703L
 	return std::reduce( first, last );
 #else
-	std::iterator_traits<InputIt>::value_type accumulator( 0 );
+	InputIt::value_type accumulator( 0 );
 	for( InputIt it(first); it != last; ++it ) [
 		accumulator += (*it);
 	]
