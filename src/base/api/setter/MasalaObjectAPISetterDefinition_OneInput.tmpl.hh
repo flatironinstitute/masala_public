83675d8ba4aa749c847fa5e48da1e30e34f2bbc7
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2025-05-19 17:11:15 -0400

    Ah, okay, that fixes the syntax issue.

--- src/base/api/setter/MasalaObjectAPISetterDefinition_OneInput.tmpl.hh
+++ src/base/api/setter/MasalaObjectAPISetterDefinition_OneInput.tmpl.hh
@@ -190,7 +190,7 @@ public:
 	void
 	set_function_deprecated () override {
 		setter_function_ = std::bind(
-			static_cast< void(MasalaObjectAPISetterDefinition::*)(T1) >( &MasalaObjectAPISetterDefinition::deprecated_function_to_bind<T1> ), this,
+			static_cast< void(MasalaObjectAPISetterDefinition::*)(T1) >( &MasalaObjectAPISetterDefinition::deprecated_function_to_bind ), this,
 			std::placeholders::_1
 		);
 	}
@@ -201,7 +201,7 @@ public:
 	set_function_warning () override {
 		std::function< void(T1) > const setter_function_copy( setter_function_ );
 		setter_function_ = std::bind(
-			static_cast< void(MasalaObjectAPISetterDefinition::*)(std::function< void(T1) >, T1) >( &MasalaObjectAPISetterDefinition::warning_function_to_bind<T1> ), this, setter_function_copy,
+			static_cast< void(MasalaObjectAPISetterDefinition::*)(std::function< void(T1) >, T1) >( &MasalaObjectAPISetterDefinition::warning_function_to_bind ), this, setter_function_copy,
 			std::placeholders::_1
 		);
 	}
