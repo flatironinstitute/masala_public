f2915ba315b96fd6dea9787605bb796d57802729
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2025-05-20 21:10:13 -0400

    Linking apps.

--- buildme.sh
+++ buildme.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir bin && echo "Created bin/ directory." || echo "Directory bin/ already exists."
+mkdir bin/tests && echo "Created bin/tests directory." || echo "Directory bin/tests already exists."
+mkdir bin/api && echo "Created bin/api directory." || echo "Directory bin/api already exists."
 mkdir build && echo "Created build/ directory." || echo "Directory build/ already exists."
 cd build
 mkdir cmake_generated && echo "Created build/cmake_generated/ sub-directory." || echo "Sub-directory build/cmake_generated/ already exists."
@@ -16,4 +18,14 @@ python3 ./code_templates/copy_headers.py masala base src headers
 python3 ./code_templates/copy_headers.py masala numeric_api src headers
 python3 ./code_templates/copy_headers.py masala core_api src headers
 python3 ./code_templates/copy_headers.py masala numeric src headers
+
+cd bin/tests
+ln -s ../../build/base_tests ./
+ln -s ../../build/numeric_tests ./
+ln -s ../../build/core_tests ./
+cd ../api
+ln -s ../../build/generate_numeric_api ./
+ln -s ../../build/generate_core_api ./
+cd ../..
+
 echo "Finished building project and copying headers."
