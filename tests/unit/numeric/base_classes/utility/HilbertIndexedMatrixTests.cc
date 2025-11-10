2d33e3c8fd17b517cc610af52c84b21afa1a67da
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2025-11-09 20:39:25 -0500

    Initial commit of unit test.

---
+++ tests/unit/numeric/base_classes/utility/HilbertIndexedMatrixTests.cc
@@ -0,0 +1,60 @@
+/*
+    Masala
+    Copyright (C) 2025 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file tests/unit/numeric/base_classes/utility/HilbertIndexedMatrixTests.cc
+/// @brief Unit tests for the Hilbert curve indexed matrix template class.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Unit testing library (Catch2) headers:
+#include <external/catch2/single_include/catch2/catch.hpp>
+
+// Unit headers:
+#include <numeric_api/base_classes/utility/HilbertIndexedMatrix.tmpl.hh>
+
+namespace masala {
+namespace tests {
+namespace unit {
+namespace numeric {
+namespace base_classes {
+namespace utility {
+
+TEST_CASE(
+	"Test the HilbertIndexedMatrix class with unsigned integer values.",
+	"[numeric_api::base_classes::utility::HilbertIndexedMatrix]"
+	"[instantiation]"
+) {
+	using masala::base::Size;
+	using namespace masala::numeric_api::base_classes::utility;
+	REQUIRE_NOTHROW([&](){
+		HilbertIndexedMatrix< Size > mat( 16, 16 );
+		Size counter( 0 );
+		for( Size col(0); col < 16; ++col ) {
+			for( Size row(0); row < 16; ++row ) {
+				mat( row, col ) = counter;
+				++counter;
+			}
+		}
+	}() );
+}
+
+} // namespace utility
+} // namespace base_classes
+} // namespace numeric
+} // namespace unit
+} // namespace tests
+} // namespace masala
